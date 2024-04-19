bbc7575731df1d1c1e262eb9a39c214be73e3eb3
Darshan.V <darshanv487@gmail.com>
2024-04-19 14:42:22 +0530

    modified transaction list component using options api

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,11 +1,30 @@
 <template>
   <h3>History</h3>
   <ul id="list" class="list">
-    <li class="minus">
+    <li v-for="transaction in transactions" :key="transaction.id" :class="transaction.amount <0? 'minus':'plus'">
+      {{ transaction.text }} <span>${{ transaction.amount }}</span
+      ><button class="delete-btn">x</button>
+    </li>
+    <!-- <li class="minus">
       Cash <span>-$400</span><button class="delete-btn">x</button>
     </li>
     <li class="plus">
       Paycheck <span>$800</span><button class="delete-btn">x</button>
-    </li>
+    </li> -->
   </ul>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      transactions: [
+        { id: 1, text: "flower", amount: -19.99 },
+        { id: 1, text: "salary", amount: 299.99 },
+        { id: 1, text: "Book", amount: -10 },
+        { id: 1, text: "Camera", amount: 150 },
+      ],
+    };
+  },
+};
+</script>
